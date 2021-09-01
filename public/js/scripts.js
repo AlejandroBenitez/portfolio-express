1e188fd8dd365d75ff0ebc2b6c6ea9a8e29d1ad7
Alejandro Benitez <alejandrobenitezcorp@outlook.com>
2021-08-31 23:48:37 -0300

    Editando los skills

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -30,3 +30,10 @@ window.addEventListener("DOMContentLoaded", (event) => {
     });
   });
 });
+
+var popoverTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="popover"]')
+);
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl);
+});
